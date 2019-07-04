2870bdb14e627518e355a6d71014e71083516f32
andreyhitoshi <andreyhitoshi>
2019-07-04 00:38:04 -0300

    autalizando

---
+++ enjoeat/features/step_definitions/restaurantes_steps.rb
@@ -0,0 +1,42 @@
+Dado("que tenho uma lista de restaurantes") do
+  @restaurant_data = [
+    {name:'Bread and Bakery', category: 'Padaria', delivery_time: '25 minutos', rating: 4.9},
+    {name:'Burguer House', category: 'Hamburgers', delivery_time: '30 minutos', rating: 3.5},
+  ]
+end
+
+Quando("acesso a lista de restaurantes") do
+    visit '/restaurants'
+
+  end
+  
+  Então("vejo todas as opções disponíveis") do
+    restaurante = all('.restaurant-item')
+    expect(restaurante.size).to be > 0
+  
+  end
+  
+  Então("cada restaurante deve exibir sua categoria") do
+    restaurants = all('.restaurant-item')
+
+    @restaurant_data.each_with_index do |value, index|
+      expect(restaurants[index]).to have_text value[:category]
+    end
+  end
+  
+  Então("cada restaurante deve exibir o tempo de entrega") do
+    restaurants = all('.restaurant-item')
+
+    @restaurant_data.each_with_index do |value, index|
+      expect(restaurants[index]).to have_text value[:delivery_time]
+    end
+  end
+  
+  Então("cada restaurante deve exibir sua nota de avaliação") do
+    restaurants = all('.restaurant-item')
+
+    @restaurant_data.each_with_index do |value, index|
+      expect(restaurants[index]).to have_text value[:rating]
+    end
+  end
+  
